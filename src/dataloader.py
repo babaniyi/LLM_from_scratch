39e1f30b0e846fb5eca30dff5944b000f7c88715
Babaniyi Olaniyi <horlaneyee@gmail.com>
2024-05-12 20:51:04 +0200

    Add position encoding to example 1 input layer

--- src/dataloader.py
+++ src/dataloader.py
@@ -62,6 +62,16 @@ pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
 max_length = 4
 dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length, stride=max_length)
 
+for batch in dataloader:
+    x, y = batch
+
+    token_embeddings = token_embedding_layer(x)
+    pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+
+    input_embeddings = token_embeddings + pos_embeddings
+
+    break
+
 
 #------------------------------------------------------
 # IMPLEMENTATION 2: Using Synthetic / random generated data
