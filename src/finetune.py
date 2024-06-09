dcc540179b24fd5c776fd827c4bdcfbca149db2a
Babaniyi Olaniyi <horlaneyee@gmail.com>
2024-06-10 00:12:48 +0200

    Introduce CoPE implementation for positional embeddings for context vectors

--- src/finetune.py
+++ src/finetune.py
@@ -98,3 +98,50 @@ def classify_review(text, model, tokenizer, device, max_length=None, pad_token_i
     predicted_label = torch.argmax(logits, dim=-1).item()
  
     return "spam" if predicted_label == 1 else "not spam"
+
+
+class CoPE(nn.Module):
+    # A novel position encoding method, that allows positions to be conditioned on context.
+    # PAPER: Contextual Position Encoding: Learning to Count What’s Important Olga Golovneva, Tianlu Wang, Jason Weston, Sainbayar Sukhbaatar
+    def __init__(self, npos_max, head_dim):
+        super().__init__ ()
+        self.npos_max = npos_max
+        self.pos_emb = nn.parameter.Parameter(torch.zeros(1 , head_dim , npos_max) )
+    
+    def forward(self , query , attn_logits ):
+        # compute positions
+        gates = torch.sigmoid(attn_logits)
+        pos = gates.flip(-1).cumsum(dim=-1).flip(-1)
+        pos = pos.clamp(max=self.npos_max - 1)
+        
+        # interpolate from integer positions
+        pos_ceil = pos.ceil().long()
+        pos_floor = pos.floor().long()
+        logits_int = torch.matmul(query, self.pos_emb)
+        logits_ceil = logits_int.gather(-1, pos_ceil)
+        logits_floor = logits_int.gather(-1, pos_floor)
+        w = pos - pos_floor
+
+        return logits_ceil * w + logits_floor * (1 - w)
+
+
+class SelfAttnCope(nn.Module):
+    def __init__(self, npos_max, head_dim):
+        super().__init__()
+        self.cope = CoPE(npos_max, head_dim)
+        self.head_dim = head_dim
+
+    def forward(self, query, key, val, mask):
+        # q, k, v have dimensions batch * seq_len * head_dim
+        attn_logits = torch.bmm(query, key.transpose(-1, -2))
+        attn_logits = attn_logits / math.sqrt(self.head_dim)
+        attn_logits += mask.log()
+        attn_logits += self.cope(query, attn_logits)
+        attn = torch.softmax(attn_logits, dim=-1)
+        out = torch.bmm(attn, val)
+
+        return out
+        
+
+    
+        
